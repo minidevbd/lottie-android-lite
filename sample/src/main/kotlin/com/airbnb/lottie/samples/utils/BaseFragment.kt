9c39ee34ff8c780e0e6abf49dc132bb4a1cc71b6
minidevbd <134840171+minidevbd@users.noreply.github.com>
2024-09-23 21:09:18 +0600

    Delete sample directory

--- sample/src/main/kotlin/com/airbnb/lottie/samples/utils/BaseFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.airbnb.lottie.samples.utils
-
-import androidx.annotation.LayoutRes
-import androidx.fragment.app.Fragment
-import com.airbnb.mvrx.MavericksView
-
-abstract class BaseFragment(@LayoutRes contentLayoutId: Int) : Fragment(contentLayoutId), MavericksView
